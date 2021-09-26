065b8273f475d783535d01f64381490acc0e5f0d
Joseph Semrai <josephsemrai@gmail.com>
2021-09-26 03:53:05 -0400

    Add video chat

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import "./styles.css";
+import { useState } from "react";
 import Timer from "./Timer";
 import WorkspaceSelector from "./WorkspaceSelector";
+import VideoChat from "./VideoChat";
+import CatalystChat from "catalyst-vc-react";
 import ReactPlayer from "react-player";
 
 export default function App() {
+  const [isVideoChatOpen, setIsVideoChatOpen] = useState(false);
   return (
     <>
-      <Timer />
+      <div className="main-container">
+        <div>
+          <Timer />
+          <WorkspaceSelector />
+          <VideoChat
+            isVideoChatOpen={isVideoChatOpen}
+            setIsVideoChatOpen={setIsVideoChatOpen}
+          />
+        </div>
 
-      <WorkspaceSelector />
+        {isVideoChatOpen && (
+          <div className="container">
+            <CatalystChat
+              room="PUBLIC_STUDY"
+              appId="XuWdJjuRRWgJpyiF8a0wmORgFut1"
+              onEndCall={() => setIsVideoChatOpen(false)}
+            />
+          </div>
+        )}
+      </div>
     </>
   );
 }
---
+++ src/VideoChat.js
@@ -0,0 +1,58 @@
+import "./videoChatStyles.css";
+
+export default function VideoChat({ isVideoChatOpen, setIsVideoChatOpen }) {
+  return (
+    <div className="container" style={{ margin: "15px" }}>
+      <div className="calculator" name="calc">
+        <div className="value" name="txt" id="display">
+          <div style={{ display: "flex" }} id="counter">
+            <p
+              style={{
+                flex: 2,
+                fontSize: 14,
+                padding: "10px",
+                textAlign: "left"
+              }}
+            >
+              Join Virtual Study Room
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                justifyItems: "center",
+                alignContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <button
+                className="start"
+                onClick={() => {
+                  setIsVideoChatOpen((isVideoChatOpen) => !isVideoChatOpen);
+                }}
+              >
+                {isVideoChatOpen ? "Close Chat" : "Join"}
+              </button>
+            </div>
+          </div>
+
+          <div
+            id="timer-select"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <button
+              className="type-select-button active"
+              style={{ marginTop: "5px" }}
+            >
+              Create a private room
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
